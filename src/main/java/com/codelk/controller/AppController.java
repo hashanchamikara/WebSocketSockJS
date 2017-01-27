f1d6b2ad4fa28d301f7ecd3a26d86f0420771caa
hashanchamikara <HC@!thinker91>
2017-01-27 11:18:15 +0530

    MessageBrokerImplemented

--- src/main/java/com/codelk/controller/AppController.java
+++ src/main/java/com/codelk/controller/AppController.java
@@ -1,7 +1,9 @@
 package com.codelk.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class AppController {
@@ -9,5 +11,10 @@ public class AppController {
 	@RequestMapping("/")
 	public String index(){
 		return "index";
+	}
+
+	@MessageMapping("/pub")
+	public String processMessage(String message){
+		return message.toUpperCase();
 	}
 }
